094d0660f9bb40a14feef8dead35df45ae58c663
JUAN FRANCISCO ZULUAGA DE LUCA <43628384@ort>
2018-10-25 09:54:26 -0300

    First commit

---
+++ TpGrupal/Controllers/JugarController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TpGrupal.Controllers
+{
+    public class JugarController : Controller
+    {
+        // GET: Jugar
+        public ActionResult Jugar()
+        {
+            return View();
+        }
+        public ActionResult JugarComoInv()
+        {
+            return View();
+        }
+    }
+}
---
+++ TpGrupal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TpGrupal.Startup))]
+namespace TpGrupal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
